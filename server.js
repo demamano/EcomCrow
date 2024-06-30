7821ce1bd08898daf7ef76d4389cde324d56c1ef
Dema Amano <demaamano@gmail.com>
2024-06-30 19:41:46 +0300

    chore: Update cors middleware to allow requests from any origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // middlewares
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
